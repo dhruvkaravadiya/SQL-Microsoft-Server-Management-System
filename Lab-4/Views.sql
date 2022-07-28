6a74402c5f60e3b87e55ec02b1de0fcc6f1f7e2e
Dhruv Karavadiya <94845676+DhruvKaravadiya@users.noreply.github.com>
2022-07-28 10:26:59 +0530

    Update Views.sql

--- Lab-4/Views.sql
+++ Lab-4/Views.sql
@@ -1,4 +1,5 @@
 
+
 --------Views
 
 --1. Create a view that display first 100 workers details.
@@ -7,6 +8,8 @@ AS
 SELECT Top 100 *
 FROM Person;
 
+--select * from VW_Display_Top100
+
 --2.Create a view that displays designation wise maximum, minimum & total salaries
 Create View VW_lab4_viewNo1
 AS
@@ -14,6 +17,7 @@ Select Max(Salary) as MaxSalary, Min(Salary) as MinSalary, Sum(Salary) as TotalS
 from Person
 Group by DesignationID;
 
+--select * from VW_lab4_viewNo1
 --3.Create a view that displays Worker’s first name with their salaries & joining date, it also displays 
 --duration column which is difference of joining date with respect to current date.
 alter View VW_lab4_viewNo2
@@ -22,10 +26,39 @@ Select
 FirstName ,
 Salary ,
 JoiningDate ,
-dateadd() as Duration
+DATEDIFF(year,  JoiningDate ,getDate()) as Duration
 from
 Person
 
-select * from  VW_lab4_viewNo2;
+--select * from  VW_lab4_viewNo2;
 
-DATEDIFF(year, '2017/08/25', '2011/08/25')
+
+
+--4.Create a view which shows department & designation wise total number of workers.
+create view  vw_Q4
+AS
+Select
+	DepartmentName, 
+	DesignationName ,
+	Count(WorkerID) AS TotalWorker 
+from  Person P 
+	inner join Department D
+on P.DepartmentID = D.DepartmentID
+	inner join Designation Ds
+on P.DesignationID = Ds.DesignationID
+GROUP BY DepartmentName,DesignationName
+
+--select * from vw_Q4
+
+--5.Create a view that displays worker names who don’t have either in any department or designation
+create view vw_Q5
+AS 
+Select FirstName + LastName as WorkerName
+from Person
+where DepartmentID = Null 
+or DesignationID = Null
+
+select * from vw_Q5
+ 
+
+
